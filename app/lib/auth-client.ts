14432764f1cde1a841a5639913ae346cca9b5484
Dev Agarwal <104455793+DevAgarwal2@users.noreply.github.com>
2025-09-14 12:53:43 +0530

    Update auth-client.ts

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://hackathon1-ejks.vercel.app",
 });
 
 export const { signIn, signOut, useSession } = authClient;
